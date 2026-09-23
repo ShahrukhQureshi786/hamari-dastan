d70797e92de128b751694508e24aecfdb9f8dc5a
ShahrukhQureshi786 <shahrukhqureshi9298952@gmail.com>
2026-09-23 13:45:46 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -5,6 +5,6 @@
   - NEVER put the Supabase service_role/secret key in GitHub/frontend files.
 */
 window.HD_SUPABASE_CONFIG = {
-  url: "YOUR_SUPABASE_PROJECT_URL",
-  publishableKey: "YOUR_SUPABASE_PUBLISHABLE_KEY"
+  url: "https://gjauqrvumtrkanxtjpbt.supabase.co",
+  publishableKey: "sb_publishable_fpMc4evW2eXeqH_g_Tr4Rg_0ZJYRBbr"
 };
